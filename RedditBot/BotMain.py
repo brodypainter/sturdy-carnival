dbf4eb0bd00a5a1e1dda75516f3f6f1b7e83f21a
brodypainter <brodypainter@email.arizona.edu>
2017-12-02 11:06:48 -0700

    Test

---
+++ RedditBot/BotMain.py
@@ -0,0 +1,12 @@
+
+import praw
+import time
+  
+r = praw.Reddit('(by sturdy-carnival)')
+ 
+###
+# Grab comments from a specific subreddit
+# 
+# Find a comment with a name of a representative
+# 
+###
